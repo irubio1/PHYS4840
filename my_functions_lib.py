7c0da3d5b82d6d8aca8f7893a16adcd1802c9c98
irubio1 <irubio@RubilYio.>
2025-02-13 16:03:16 -0700

    added distance_modulus and format_axes

--- my_functions_lib.py
+++ my_functions_lib.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3.8
 
 import numpy as np
+import matplotlib.pyplot as plt
+
 
 def my_function(vector):
 	a = vector[0]
@@ -26,3 +28,21 @@ def greatest_common_divisor(m,n):
 def y(x):
 	y = 2.0*x**3.0
 	return y
+
+def distance_modulus(d):
+	distance_modulus = 5*np.log10(d/10)
+
+	return distance_modulus
+		
+
+
+
+	
+	#this is in pc
+
+	return d
+
+def format_axes(ax):
+    ax.tick_params(axis='both', which='major', labelsize=14, length=6, width=1.5)  # Larger major ticks
+    ax.tick_params(axis='both', which='minor', labelsize=12, length=3, width=1)    # Minor ticks
+    ax.minorticks_on()  # Enable minor ticks
